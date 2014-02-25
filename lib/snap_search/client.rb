699fa6bf03c6a99eadd590880cc84046bfbb1cf8
Ryan Scott Lewis <ryan@rynet.us>
2014-02-25 16:34:38 -0500

    cs cert file option on the Client

--- lib/snap_search/client.rb
+++ lib/snap_search/client.rb
@@ -9,7 +9,7 @@ module SnapSearch
     # field from the JSON response body.
     class Client
         
-        attr_reader :email, :key, :parameters, :api_url
+        attr_reader :email, :key, :parameters, :api_url, :ca_cert_file
         
         # Create a new Client instance.
         # 
@@ -18,6 +18,7 @@ module SnapSearch
         # @option options [String, #to_s] :key The secret authentication key.
         # @option options [Hash, #to_h] :parameters ({}) The parameters to send with the HTTP request.
         # @option options [String, #to_s] :api_url (https://snapsearch.io/api/v1/robot) The URL to send the HTTP request to.
+        # @option options [String, #to_s] :ca_cert_file (ROOT/resources/cacert.pem) The CA cert file to use with request.
         def initialize(options={})
             initialize_attributes(options)
         end
@@ -64,6 +65,16 @@ module SnapSearch
             @api_url = value.to_s
         end
         
+        # Validate and set the value of the `ca_cert_file` attribute.
+        # 
+        # @param [String, #to_s] value The value to set the attribute to.
+        # @return [String] The new value of the attribute.
+        def ca_cert_file=(value)
+            raise TypeError, 'ca_cert_file must be a String or respond_to #to_s' unless value.is_a?(String) || value.respond_to?(:to_s)
+            
+            @ca_cert_file = value.to_s
+        end
+        
         # Send an authenticated HTTP request to the `api_url` and return the `content` field from the JSON response body.
         # 
         # @param [String, #to_s] url The url to send in the parameters to the `api_url`.
@@ -84,16 +95,18 @@ module SnapSearch
         # @option options [String, #to_s] :key The secret authentication key.
         # @option options [Hash, #to_h] :parameters ({}) The parameters to send with the HTTP request.
         # @option options [String, #to_s] :api_url (https://snapsearch.io/api/v1/robot) The URL to send the HTTP request to.
+        # @option options [String, #to_s] :ca_cert_file (ROOT/resources/cacert.pem) The CA cert file to use with request.
         def initialize_attributes(options)
             raise TypeError, 'options must be a Hash or respond to #to_h' unless options.is_a?(Hash) || options.respond_to?(:to_h) || options.respond_to?(:to_hash)
             options = options.to_h rescue options.to_hash
             
             options = {
                 parameters: {},
-                api_url: 'https://snapsearch.io/api/v1/robot'
+                api_url: 'https://snapsearch.io/api/v1/robot',
+                ca_cert_file: Pathname.new(__FILE__).join('..', '..', '..', 'resources', 'cacert.pem').to_s
             }.merge(options.to_h)
             
-            self.email, self.key, self.parameters, self.api_url = options.values_at(:email, :key, :parameters, :api_url)
+            self.email, self.key, self.parameters, self.api_url, self.ca_cert_file = options.values_at(:email, :key, :parameters, :api_url, :ca_cert_file)
         end
         
         # Send an authenticated HTTP POST request encoded in JSON to the API URL
@@ -104,7 +117,7 @@ module SnapSearch
             request = HTTPI::Request.new
             request.url = api_url
             request.auth.basic(email, key)
-            request.auth.ssl.ca_cert_file = Pathname.new(__FILE__).join('..', '..', '..', 'resources', 'cacert.pem').to_s
+            request.auth.ssl.ca_cert_file = ca_cert_file
             request.auth.ssl.verify_mode = :peer
             request.open_timeout = 30
             request.headers['Content-Type'] = 'application/json'
